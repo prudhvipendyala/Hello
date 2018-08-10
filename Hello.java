080169645d0afa149ece49bdb1447add3fea830e
prudhvipendyala <prudhvi.184u@gmail.com>
2018-08-10 16:01:29 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String [] arg) {
 		for (int i = 0; i < 10; i++) {
-			System.out.println("Hi this is prudhvi id : "+ i);
+			System.out.println("Hi this is prudhvi ids : "+ i);
 		}
 		
 	}
