a6be69f18f50e5b0c5225cede129b83ac4cf9bcf
prudhvi.pendyala <prudhvi.pendyala@leoforce.local>
2018-12-18 23:02:12 +0530

    change program

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,10 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Testing for first jenkins job");
+for (int i = 0; i < 10; i++) {
+			System.out.println("Hi ,this is prudhvi ids are : "+ i);
+
+		}
 	}
 
 }
